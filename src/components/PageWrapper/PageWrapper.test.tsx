fa4a35c24f2a6d19df1f8ca57bba938062ea9de9
Danielle Souza <dany.sa1@hotmail.com>
2025-04-20 12:32:01 -0300

    feat: add page test

---
+++ src/components/PageWrapper/PageWrapper.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { expect } from "vitest";
+import PageWrapper from "./PageWrapper";
+import { BrowserRouter } from "react-router";
+
+describe("PageWrapper component", () => {
+  test("renderiza os textos do header, rodapé e dos componentes filhos!", async () => {
+    render(
+      <BrowserRouter>
+        <PageWrapper>
+          <h1>Título</h1>
+        </PageWrapper>
+      </BrowserRouter>
+    );
+
+    const header = screen.getByText("Home");
+    const footer = screen.getByText("Github - Danielle de Souza");
+    const text = screen.getByText("Título");
+
+    expect(header).toBeVisible();
+    expect(footer).toBeVisible();
+    expect(text).toBeVisible();
+
+  });
+});
